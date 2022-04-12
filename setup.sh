90aed5759755004f01e88191bb3fcc19fd1f90f1
Nick Fox-Gieg <nick@fox-gieg.com>
2022-04-12 01:09:56 -0400

    shell

--- setup.sh
+++ setup.sh
@@ -11,12 +11,12 @@ cd pytorch-install
 # TORCH
 wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 sudo apt-get install python3-pip libopenblas-base libopenmpi-dev libomp-dev
-pip3 install Cython
-pip3 install numpy torch-1.10.0-cp36-cp36m-linux_aarch64.whl 
+pip3 install numpy cython
+pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl 
 
 # TORCHVISION
 sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
-git clone --branch <version> https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
+git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
 cd torchvision
 export BUILD_VERSION=0.11.1  # where 0.x.0 is the torchvision version  
 python3 setup.py install --user
@@ -35,11 +35,11 @@ sudo python3 setup.py install --plugins
 
 cd $DIR
 sudo apt-get install python3-matplotlib libhdf5-dev libffi-dev
-sudo pip3 install tqdm cython pycocotools 
+pip3 install tqdm pycocotools 
 # jupyter notebook
-sudo pip3 install pandas numpy pillow
-#sudo pip3 h5py scipy scikit-image scikit-learn 
-sudo pip3 install cffi jupyter ipywidgets packaging notebook jupyterlab
+pip3 install pandas pillow
+#pip3 install h5py scipy scikit-image scikit-learn 
+pip3 install cffi jupyter ipywidgets packaging notebook jupyterlab
 
 sudo python3 setup.py install
 
